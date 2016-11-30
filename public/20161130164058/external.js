d458c994dec535f0599d3a30f899d395f10a86b0
Julián Andrés López Valencia <jlopez@tredasolutions.com>
2016-11-30 17:24:15 -0500

    Updating neccesary files

---
+++ public/20161130164058/external.js
@@ -0,0 +1,11 @@
+content/lib/jquery-2.1.3.min.js
+content/lib/underscore-1.4.4.min.js
+content/lib/bootstrap-2.3.1.min.js
+content/lib/jquery-ui-1.10.0.custom.min.js
+content/lib/color-0.4.1.min.js
+content/lib/jquery.colorPicker.min.js
+content/lib/hammer-1.1.3.min.js
+content/lib/jquery.hammer-1.1.3.min.js
+content/lib/jquery.hotkeys.js
+content/lib/google-media-uploader-20140522.js
+
