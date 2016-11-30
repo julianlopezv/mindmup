119e74a166a9b915b100e0c0c00b9f66eed4921d
Julián Andrés López Valencia <jlopez@tredasolutions.com>
2016-11-30 17:22:18 -0500

    Adding more files and changing static ts path

---
+++ public/embedded_external.js
@@ -0,0 +1,5 @@
+content/lib/jquery-2.1.3.min.js
+content/lib/underscore-1.4.4.min.js
+content/lib/color-0.4.1.min.js
+content/lib/jquery.hammer.min.js
+content/lib/jquery.hotkeys.js
---
+++ public/external-ios.js
@@ -0,0 +1,7 @@
+content/lib/jquery-2.1.3.min.js
+content/lib/underscore-1.4.4.min.js
+content/lib/jquery-ui-1.10.0.custom.min.js
+content/lib/color-0.4.1.min.js
+content/lib/hammer-1.1.3.min.js
+content/lib/jquery.hammer-1.1.3.min.js
+content/lib/jquery.hotkeys.js
